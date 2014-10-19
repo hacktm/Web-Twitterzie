4b37b9b6edd00f941b510e15e9c23d7d80992a38
Adi Balcan <ady_1689@yahoo.com>
2014-10-19 12:41:22 +0300

    added image

---
+++ Twitterzie/twiter pull/refilter.js
@@ -0,0 +1,24 @@
+var redis = require('./RedisPush.js');
+var parser = require('./parse_tweets.js');
+var initial = require('../genetic engine/testInput.js');
+
+
+addFlitered = function(tweets){
+	for(i in tweets){
+		var parts = parser.split(tweets[i]);
+		for(i in parts){
+			part = parser.preetyTweet(parts[i]);
+			//Check if is valid
+			if(part){
+				//Add in a second collection
+				redis.addFinal(part);
+			}
+		}		
+	}
+}
+
+addFlitered(initial);
+
+redis.allVerse(function(err, results){
+	addFlitered(results);
+});
